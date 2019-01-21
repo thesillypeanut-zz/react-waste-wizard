390fde54b0a4fdb41eb58dd9eb065b0cade7e0ee
Maliha Islam <maliha.rh@gmail.com>
2019-01-21 03:04:39 -0500

    Start with create-react-app

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { AppHeader } from './react-components/app-header';
+import { BaseReactComponent } from './react-components/base-react-component';
+import { Favourites } from './react-components/favourites';
+import { SearchBar } from './react-components/search-bar';
+import { SearchResults } from './react-components/search-results';
+
+import './styles.css';
+
+
+export default class App extends BaseReactComponent {
+  filterState() {
+    return {};
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <AppHeader />
+        <section className="app__container">
+          <div className="app__search">
+            <SearchBar />
+            <SearchResults />
+          </div>
+          <Favourites />
+        </section>
+      </div>
+    );
+  }
+};
